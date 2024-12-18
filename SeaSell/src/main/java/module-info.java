8fb30153f2b56f29dbeece8f664acd275fc8b359
Ali Chandra Rizki <153173069+XT32@users.noreply.github.com>
2024-12-19 06:19:53 +0700

    Delete SeaSell directory

--- SeaSell/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.seasell {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires java.base;
-    
-    opens com.mycompany.seasell to javafx.fxml;
-    exports com.mycompany.seasell;
-}
